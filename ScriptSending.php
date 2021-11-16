6235b00d2e31234b20f5e96aa78d5fb67fbd6e20
chentouf bilal <94446034+chentoufbilal007@users.noreply.github.com>
2021-11-16 12:32:06 +0100

    Update ScriptSending.php

--- ScriptSending.php
+++ ScriptSending.php
@@ -22,8 +22,8 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'chentoufbilal.pro@gmail.com';                     //SMTP username
-    $mail->Password   = 'bilal2015';                               //SMTP password
+    $mail->Username   = 'your email';                     //SMTP username
+    $mail->Password   = ' your password';                               //SMTP password
     $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
     $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
     $rev=$_POST['emailz'];
